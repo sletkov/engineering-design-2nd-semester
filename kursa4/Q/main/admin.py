96b57d84d7e7e752ce8c0f44c8415b4ba6787318
sletkov <70477360+sletkov@users.noreply.github.com>
2021-07-10 19:09:19 +0300

    Обновил проект, перенес его в main

--- kursa4/Q/main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Feedback
-from .models import News
-from .models import Question
-
-
-admin.site.register(Feedback)
-admin.site.register(News)
-admin.site.register(Question)
